0eebd39fd1ffdaa401183a02974c8579073c17b0
Jan Godziatkowski <ph.godzio@gmail.com>
2015-10-18 18:26:15 +0200

    Add serializer and deserializer for LocalDateTime (Java <-> Json)

---
+++ src/main/java/pl/pszczolkowski/mustdo/web/converter/LocalDateTimeDeserializer.java
@@ -0,0 +1,25 @@
+package pl.pszczolkowski.mustdo.web.converter;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+public class LocalDateTimeDeserializer
+   extends JsonDeserializer<LocalDateTime> {
+
+   @Override
+   public LocalDateTime deserialize(JsonParser jp,
+      DeserializationContext context)
+      throws IOException {
+      if (jp != null) {
+         return LocalDateTime.parse(jp.getText(), DateTimeFormatter.ISO_DATE_TIME);
+      } else {
+         return null;
+      }
+   }
+
+}
---
+++ src/main/java/pl/pszczolkowski/mustdo/web/converter/LocalDateTimeSerializer.java
@@ -0,0 +1,25 @@
+package pl.pszczolkowski.mustdo.web.converter;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+public class LocalDateTimeSerializer
+   extends JsonSerializer<LocalDateTime> {
+
+   private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+   @Override
+   public void serialize(LocalDateTime value, JsonGenerator jgen,
+      SerializerProvider provider)
+      throws IOException {
+      if (jgen != null) {
+         jgen.writeString(value.format(FORMATTER));
+      }
+   }
+
+}
