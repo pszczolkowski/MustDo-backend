20bd2f51a9f123d3ba69df6339a43b9205a647d6
Damian Spinek <damian00737@gmail.com>
2015-10-24 19:15:18 +0200

    Add tests to TasksList entity

---
+++ src/test/java/pl/pszczolkowski/mustdo/domain/task/domain/steps/GivenTasksList.java
@@ -0,0 +1,51 @@
+package pl.pszczolkowski.mustdo.domain.task.domain.steps;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+
+import pl.pszczolkowski.mustdo.domain.board.dto.BoardSnapshot;
+import pl.pszczolkowski.mustdo.domain.board.entity.Board;
+import pl.pszczolkowski.mustdo.domain.board.repository.BoardRepository;
+import pl.pszczolkowski.mustdo.domain.task.entity.TasksList;
+import pl.pszczolkowski.mustdo.domain.task.repository.TasksListRepository;
+
+public class GivenTasksList extends Stage<GivenTasksList>{
+	
+	@ExpectedScenarioState
+	private BoardRepository boardRepository;
+	@ExpectedScenarioState
+	private TasksListRepository tasksListRepository;
+	
+	@ProvidedScenarioState
+	private  String name = GivenTasksList.class.getSimpleName();
+	@ProvidedScenarioState
+	private BoardSnapshot boardSnapshot;
+	@ProvidedScenarioState
+	private TasksList tasksList;
+	
+	public GivenTasksList a_board() {
+		Board board = new Board(name);
+		board = boardRepository.save(board);
+		boardSnapshot = board.toSnapshot();
+		return this;
+	}
+
+	public GivenTasksList a_not_persisted_tasks_list() {
+		tasksList = new TasksList(name, boardSnapshot.getId());
+		return this;
+	}
+
+	public GivenTasksList a_persistet_tasks_list() {
+		tasksList = new TasksList(name, boardSnapshot.getId());
+		tasksList = tasksListRepository.save(tasksList);
+		return this;
+		
+	}
+
+	public void a_persisted_tasks_list_with_name(String name) {
+		tasksList = new TasksList(name, boardSnapshot.getId());
+		tasksList = tasksListRepository.save(tasksList);
+	}
+
+}
