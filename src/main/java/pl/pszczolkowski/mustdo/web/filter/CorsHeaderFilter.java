ec6f25c2694517c16d81074526a87ed504983d81
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2015-11-12 11:31:51 +0100

    Improve CORS header filter

--- src/main/java/pl/pszczolkowski/mustdo/web/filter/CorsHeaderFilter.java
+++ src/main/java/pl/pszczolkowski/mustdo/web/filter/CorsHeaderFilter.java
@@ -25,9 +25,9 @@ public class CorsHeaderFilter implements Filter {
 		HttpServletResponse response = (HttpServletResponse) res;
 		HttpServletRequest request = (HttpServletRequest) req;
 		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization");
+		response.setHeader("Access-Control-Allow-Headers", "content-type, x-requested-with, authorization");
 
 		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
 			response.setStatus(HttpServletResponse.SC_OK);
