8c65fafb5b1da3ded7e1cef3cb08fcba31657742
Jan Godziatkowski <ph.godzio@gmail.com>
2015-11-14 11:28:28 +0100

    Update TasksList entity to store data about linked Tasks

--- src/main/java/pl/pszczolkowski/mustdo/domain/task/entity/TasksList.java
+++ src/main/java/pl/pszczolkowski/mustdo/domain/task/entity/TasksList.java
@@ -1,14 +1,22 @@
 package pl.pszczolkowski.mustdo.domain.task.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import pl.pszczolkowski.mustdo.domain.task.dto.TaskSnapshot;
 import pl.pszczolkowski.mustdo.domain.task.dto.TasksListSnapshot;
 import pl.pszczolkowski.mustdo.sharedkernel.exception.EntityInStateNewException;
 
@@ -29,7 +37,15 @@ public class TasksList
    @Size(min = 2, max = 100)
    private String name;
 
+   @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "tasksList", orphanRemoval = true)
+   @OrderBy("position")
+   private List<Task> tasks = new ArrayList<>();
+
    protected TasksList() {
+   }
+
+   int taskCount() {
+      return tasks.size();
    }
 
    public TasksList(String name, Long boardId) {
@@ -41,11 +57,20 @@ public class TasksList
       this.name = name;
    }
 
+   public void addTask(Task task) {
+      tasks.add(task);
+   }
+
    public TasksListSnapshot toSnapshot() {
       if (id == null) {
          throw new EntityInStateNewException();
       }
-      return new TasksListSnapshot(id, boardId, name);
+      List<TaskSnapshot> taskSnapshots = tasks
+         .stream()
+         .map(Task::toSnapshot)
+         .collect(Collectors.toList());
+
+      return new TasksListSnapshot(id, boardId, name, taskSnapshots);
    }
 
 }
