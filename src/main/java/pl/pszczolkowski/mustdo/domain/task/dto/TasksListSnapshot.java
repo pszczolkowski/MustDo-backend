f5e32fefd0a47626573e55c361b4a040ef121e47
Jan Godziatkowski <ph.godzio@gmail.com>
2015-11-14 11:21:42 +0100

    Update TasksListSnapshot due to addition of linked Tasks data in TasksList entity

--- src/main/java/pl/pszczolkowski/mustdo/domain/task/dto/TasksListSnapshot.java
+++ src/main/java/pl/pszczolkowski/mustdo/domain/task/dto/TasksListSnapshot.java
@@ -1,15 +1,19 @@
 package pl.pszczolkowski.mustdo.domain.task.dto;
 
+import java.util.List;
+
 public class TasksListSnapshot {
 
    private final Long id;
    private final Long boardId;
    private final String name;
+   private final List<TaskSnapshot> taskSnapshots;
 
-   public TasksListSnapshot(Long id, Long boardId, String name) {
+   public TasksListSnapshot(Long id, Long boardId, String name, List<TaskSnapshot> taskSnapshots) {
       this.id = id;
       this.boardId = boardId;
       this.name = name;
+      this.taskSnapshots = taskSnapshots;
    }
 
    public Long getId() {
@@ -22,6 +26,10 @@ public class TasksListSnapshot {
 
    public String getName() {
       return name;
+   }
+
+   public List<TaskSnapshot> getTaskSnapshots() {
+      return taskSnapshots;
    }
 
 }
