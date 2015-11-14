968c86239017fb619c4eebe7b3784cd723d28c05
Jan Godziatkowski <ph.godzio@gmail.com>
2015-11-14 11:23:20 +0100

    Create Business Object for Task

---
+++ src/main/java/pl/pszczolkowski/mustdo/domain/task/bo/TaskBO.java
@@ -0,0 +1,15 @@
+package pl.pszczolkowski.mustdo.domain.task.bo;
+
+import pl.pszczolkowski.mustdo.domain.task.dto.TaskSnapshot;
+
+public interface TaskBO {
+
+   TaskSnapshot add(Long tasksListId, Long boardId, String title, String description);
+
+   void moveToAntoherTasksList(Long id, Long tasksListId);
+
+   void edit(Long id, String title, String description);
+
+   void delete(Long id);
+
+}
