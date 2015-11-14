07752e84ce3c1a21737d709f2ff5db70123f6539
Jan Godziatkowski <ph.godzio@gmail.com>
2015-11-14 11:23:58 +0100

    Create Task finder

---
+++ src/main/java/pl/pszczolkowski/mustdo/domain/task/finder/TaskSnapshotFinder.java
@@ -0,0 +1,15 @@
+package pl.pszczolkowski.mustdo.domain.task.finder;
+
+import java.util.List;
+
+import pl.pszczolkowski.mustdo.domain.task.dto.TaskSnapshot;
+
+public interface TaskSnapshotFinder {
+   
+   TaskSnapshot findOneById(Long id);
+   
+   List<TaskSnapshot> findAllByTasksListId( Long tasksListId);
+   
+   List<TaskSnapshot> findAllByBoardId(Long boardId);
+
+}
---
+++ src/main/java/pl/pszczolkowski/mustdo/domain/task/finder/TaskSnapshotFinderImpl.java
@@ -0,0 +1,47 @@
+package pl.pszczolkowski.mustdo.domain.task.finder;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import pl.pszczolkowski.mustdo.domain.task.dto.TaskSnapshot;
+import pl.pszczolkowski.mustdo.domain.task.entity.Task;
+import pl.pszczolkowski.mustdo.domain.task.repository.TaskRepository;
+import pl.pszczolkowski.mustdo.sharedkernel.annotations.Finder;
+
+@Finder
+public class TaskSnapshotFinderImpl
+   implements TaskSnapshotFinder {
+
+   private final TaskRepository taskRepository;
+
+   @Autowired
+   public TaskSnapshotFinderImpl(TaskRepository taskRepository) {
+      this.taskRepository = taskRepository;
+   }
+
+   @Override
+   public TaskSnapshot findOneById(Long id) {
+      Task task = taskRepository.findOne(id);
+
+      return task == null ? null : task.toSnapshot();
+   }
+
+   @Override
+   public List<TaskSnapshot> findAllByTasksListId(Long tasksListId) {
+      return taskRepository
+         .findAllByTasksListId(tasksListId)
+         .stream().map(Task::toSnapshot)
+         .collect(Collectors.toList());
+   }
+
+   @Override
+   public List<TaskSnapshot> findAllByBoardId(Long boardId) {
+      return taskRepository
+         .findAllByBoardId(boardId)
+         .stream().map(Task::toSnapshot)
+         .collect(Collectors.toList());
+   }
+
+}
